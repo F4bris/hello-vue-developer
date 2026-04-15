3ede8e9fc43df56ed00380287fc8a3bae15c8cf8
F4bris <114889596+F4bris@users.noreply.github.com>
2026-04-15 13:41:28 -0500

    feat(developer-registration): add developer registration functionality and update developer count

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,39 @@
 <script setup>
 
+  import {ref} from "vue";
+  import {Developer} from "./greetings/model/components/Developer";
+
+  const firstName = ref('');
+  const lastName = ref('');
+  const developerCount = ref(0);
+  const hasRegistered= ref(false);
+
+  function updatedRegisteredDeveloperInfo(developer){
+    firstName.value = developer.firstName;
+    lastName.value = developer.lastName;
+    hasRegistered.value = true;
+    console.log('Developer registered: ', developer);
+    //TODO: Update developer info
+  }
+
+  function resetRegisteredDeveloperInfo(){
+    firstName.value = '';
+    lastName.value = '';
+    hasRegistered.value = false;
+    console.log('Registration deferred');
+  }
+
+  function updateDeveloperCount(developer){
+    const dev = new Developer(developer.firstName, developer.lastName);
+    if(dev.fullName !== 'Unknown'){
+      developerCount.value++;
+    }
+  }
 </script>
 
 <template>
+  <h1>Hello Vue Developer Application</h1>
+  <developer-registration @developer-registered ="updatedRegisteredDeveloperInfo"
+  @registration-deferred="resetRegisteredDeveloperInfo"/>
+
 </template>
